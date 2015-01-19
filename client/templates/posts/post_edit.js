a94b76f5a3db0f9c60884d0696657b563751ff94
aksharbonu <akshar.bonu@gmail.com>
2015-01-19 19:32:57 +0800

    more robust editing (cannot add duplicate url)

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -9,13 +9,16 @@ Template.postEdit.events({
       title: $(e.target).find('[name=title]').val()
     }
 
-    Posts.update(currentPostId, {$set: postProperties}, function(error) {
-      if (error) {
-        // display the error to the user
-        alert(error.reason);
-      } else {
-        Router.go('postPage', {_id: currentPostId});
-      }
+    Meteor.call('postEdit', postProperties, currentPostId, function(error, result) {
+      // display the error to the user and abort
+      if (error)
+        return alert(error.reason);
+    
+      // show this result but route anyway
+      if (result.postExists)
+        alert('This link has already been posted');
+
+      Router.go('postPage', {_id: result._id});  
     });
   },
 
