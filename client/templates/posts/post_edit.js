96bf7093c17fff7d066108c043724df01cc44a60
aksharbonu <akshar.bonu@gmail.com>
2015-01-21 15:09:57 +0800

    Added routes for post lists, and pages to display them

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -47,7 +47,7 @@ Template.postEdit.events({
     if (confirm("Delete this post?")) {
       var currentPostId = this._id;
       Posts.remove(currentPostId);
-      Router.go('postsList');
+      Router.go('home');
     }
   }
 });
